59a2f85ae3be400cdfb8128f1d044e7e7a397b2e
Laroub03 <ayoublaroub@gmail.com>
2023-08-08 23:26:05 +0200

    Major changes
    Code comment + SOLID

---
+++ ConsoleUserInterface.cs
@@ -0,0 +1,12 @@
+﻿using SafeStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeStorage
+{ 
+    // Implement the console user interface  
+        
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,107 @@
 ﻿using System;
+using System.Security.Cryptography;
 
-namespace CryptographyInDotNet
+namespace SafeStorage
 {
-    public class Program
+    // Define interfaces for salt generation, key generation, and password validation
+    public interface ISaltGenerator
     {
-        static void DisplayMenu()
+        byte[] GenerateSalt();
+    }
+
+    public interface IKeyGenerator
+    {
+        byte[] GenerateKey(string password, byte[] salt, int iterations);
+    }
+
+    public interface IPasswordValidator
+    {
+        bool ValidatePassword(string originalPassword, string storedSaltBase64, string storedKeyBase64, int iterations);
+    }
+
+    // Implement the PBKDF2SaltGenerator using RNGCryptoServiceProvider
+    public class PBKDF2SaltGenerator : ISaltGenerator
+    {
+        public byte[] GenerateSalt()
+        {
+            using var rng = new RNGCryptoServiceProvider();
+            byte[] salt = new byte[16];
+            rng.GetBytes(salt);
+            return salt;
+        }
+    }
+
+    // Implement the PBKDF2KeyGenerator using Rfc2898DeriveBytes
+    public class PBKDF2KeyGenerator : IKeyGenerator
+    {
+        public byte[] GenerateKey(string password, byte[] salt, int iterations)
+        {
+            using var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            return pbkdf2.GetBytes(32);
+        }
+    }
+
+    // Implement the PBKDF2PasswordValidator using key generator
+    public class PBKDF2PasswordValidator : IPasswordValidator
+    {
+        private readonly IKeyGenerator _keyGenerator;
+
+        public PBKDF2PasswordValidator(IKeyGenerator keyGenerator)
+        {
+            _keyGenerator = keyGenerator;
+        }
+
+        public bool ValidatePassword(string originalPassword, string storedSaltBase64, string storedKeyBase64, int iterations)
+        {
+            byte[] storedSalt = Convert.FromBase64String(storedSaltBase64);
+            byte[] storedKey = Convert.FromBase64String(storedKeyBase64);
+
+            byte[] generatedKey = _keyGenerator.GenerateKey(originalPassword, storedSalt, iterations);
+
+            return CompareByteArrays(storedKey, generatedKey);
+        }
+
+        private static bool CompareByteArrays(byte[] array1, byte[] array2)
+        {
+            if (array1.Length != array2.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    // Implement the console user interface
+    public class ConsoleUserInterface
+    {
+        private readonly ISaltGenerator _saltGenerator;
+        private readonly IKeyGenerator _keyGenerator;
+        private readonly IPasswordValidator _passwordValidator;
+
+        public ConsoleUserInterface(ISaltGenerator saltGenerator, IKeyGenerator keyGenerator, IPasswordValidator passwordValidator)
+        {
+            _saltGenerator = saltGenerator;
+            _keyGenerator = keyGenerator;
+            _passwordValidator = passwordValidator;
+        }
+
+        // Helper method to convert base64 string to byte array
+        private byte[] ConvertBase64ToBytes(string base64)
+        {
+            return Convert.FromBase64String(base64);
+        }
+
+        // Display the main menu
+        private void DisplayMenu()
         {
             Console.WriteLine("==================================================");
             Console.WriteLine("                  Login Page");
@@ -15,7 +112,8 @@ namespace CryptographyInDotNet
             Console.WriteLine("Insert choice");
         }
 
-        static int GetUserChoice()
+        // Get user's choice from the menu
+        private int GetUserChoice()
         {
             int choice;
             while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 3)
@@ -25,7 +123,35 @@ namespace CryptographyInDotNet
             return choice;
         }
 
-        static void Login()
+        // Get masked password input from the user
+        private string GetMaskedPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo key;
+
+            do
+            {
+                key = Console.ReadKey(true); // Read a key without showing it
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    password += key.KeyChar;
+                    Console.Write("*"); // Display asterisk
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                    {
+                        password = password.Remove(password.Length - 1);
+                        Console.Write("\b \b"); // Clear the last character
+                    }
+                }
+            } while (key.Key != ConsoleKey.Enter);
+
+            return password;
+        }
+
+        // Handle user login
+        private void Login()
         {
             Console.WriteLine("Enter username: ");
             string username = Console.ReadLine();
@@ -33,16 +159,23 @@ namespace CryptographyInDotNet
             Console.WriteLine("Enter password: ");
             string password = GetMaskedPassword();
 
-            // Hent brugerens gemte salt og nøgle fra en sikker lagermekanisme
-            string storedSaltBase64 = "saved_salt_base64_here"; // Erstatt med faktiske gemte værdier
-            string storedKeyBase64 = "saved_key_base64_here";
+            byte[] salt = _saltGenerator.GenerateSalt();
+            string saltBase64 = Convert.ToBase64String(salt);
 
-            // Valider adgangskoden
-            bool isValidPassword = PBKDF2.ValidatePassword(password, storedSaltBase64, storedKeyBase64, iterations);
+            byte[] key = _keyGenerator.GenerateKey(password, salt, 10000);
+            string keyBase64 = Convert.ToBase64String(key);
+
+            bool isValidPassword = _passwordValidator.ValidatePassword(password, saltBase64, keyBase64, 10000);
 
             if (isValidPassword)
             {
+                Console.WriteLine();
                 Console.WriteLine("Login successful.");
+                Console.WriteLine();
+                Console.WriteLine("Username: " + username);
+                Console.WriteLine("Password: " + password);
+                Console.WriteLine("Salt: " + saltBase64);
+                Console.WriteLine("Key: " + keyBase64);
             }
             else
             {
@@ -50,7 +183,8 @@ namespace CryptographyInDotNet
             }
         }
 
-        static void ChangePassword()
+        // Handle password change
+        private void ChangePassword()
         {
             Console.WriteLine("Enter username: ");
             string username = Console.ReadLine();
@@ -61,27 +195,18 @@ namespace CryptographyInDotNet
             Console.WriteLine("Enter new password: ");
             string newPassword = GetMaskedPassword();
 
-            // Hent brugerens gemte salt fra en sikker lagermekanisme
-            string storedSaltBase64 = "saved_salt_base64_here"; // Erstatt med faktiske gemte værdier
-
-            // Generer ny nøgle og salt for den nye adgangskode
-            byte[] newSalt = PBKDF2.GenerateSalt();
+            byte[] newSalt = _saltGenerator.GenerateSalt();
             string newSaltBase64 = Convert.ToBase64String(newSalt);
-            byte[] newKey = PBKDF2.GeneratePBKDF2Key(newPassword, newSalt, iterations);
-            string newKeyBase64 = Convert.ToBase64String(newKey);
 
-            // Opdater brugerens gemte salt og nøgle med de nye værdier i lageret
+            byte[] newKey = _keyGenerator.GenerateKey(newPassword, newSalt, 10000);
+            string newKeyBase64 = Convert.ToBase64String(newKey);
 
             Console.WriteLine("Password changed successfully.");
         }
 
-
-        static void Main()
+        // Main program loop
+        public void Run()
         {
-            int iterations = 10000;
-            byte[] salt = PBKDF2.GenerateSalt();
-            string saltBase64 = Convert.ToBase64String(salt);
-
             while (true)
             {
                 DisplayMenu();
@@ -102,6 +227,22 @@ namespace CryptographyInDotNet
                         break;
                 }
             }
+        }
+    }
+
+    // Entry point of the program
+    class Program
+    {
+        static void Main()
+        {
+            // Initialize necessary components
+            var saltGenerator = new PBKDF2SaltGenerator();
+            var keyGenerator = new PBKDF2KeyGenerator();
+            var passwordValidator = new PBKDF2PasswordValidator(keyGenerator);
+
+            // Create and run the console user interface
+            var userInterface = new ConsoleUserInterface(saltGenerator, keyGenerator, passwordValidator);
+            userInterface.Run();
         }
     }
 }
